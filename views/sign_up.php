e85c51ca3b40583412731cfa6fb30356312d57ce
Ned <nedparrylloyd@gmail.com>
2018-02-11 15:55:01 +0000

    added pages

--- views/sign_up.php
+++ views/sign_up.php
@@ -1,12 +1,11 @@
 <?php
-
 $connectstr_dbhost = "group21-mysql-server.mysql.database.azure.com";
 $connectstr_dbusername = "group21@group21-mysql-server";
 $connectstr_dbname = 'online_auction_db';
 $connectstr_dbpassword = 'COMPGC06@@';
 
 $con=mysqli_init();
-// mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);
+
 mysqli_real_connect($con, $connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname, 3306);
 ?>
 
