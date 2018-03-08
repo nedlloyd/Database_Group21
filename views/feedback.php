5345b6120e378c50953c6a0c4c054a737e944a1d
Yi-Hsuan CHIANG <bb33882011@gmail.com>
2018-03-08 14:15:42 +0000

    feedback

--- views/feedback.php
+++ views/feedback.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 require '../assets/php/connect.php';
-echo $_SESSION['userID'];
-echo $_SESSION['role'];
 ?>
 
 
@@ -36,88 +34,57 @@ echo $_SESSION['role'];
 
 
 </head>
+<meta charset="utf-8">
+<title>Feedback</title>
+</head>
 <body>
-    <div class="wrapperforstickyfooter">
-  <div class="content-wrap">
-    <div class="container">
-
-    <form class="form-horizontal" method="post" action="do.php">
-      <fieldset>
-        <legend>Feedback</legend>
-              <p></p>
-
+    <form method="POST" enctype="multipart/form-data">       <!-- form delect action="do.php", add method-->                             
         <div class="form-group">
-          <label for="userID" class="col-sm-4 control-label">userID</label>
-          <div class="col-sm-4">
-            <input name="userID" type="text" class="form-control" columns="7" id="userID" placeholder="userID">
-            <span class="error"></span>
-          </div>
-        </div>
-
+          <label for="userID" class="col-sm-3 control-label"></label>
+        <div class="col-sm-3">
         <div class="form-group">
-          <label for="email" class="col-sm-4 control-label">email</label>
-          <div class="col-sm-4">
-            <input name="email" type="text" class="form-control" id="email" placeholder="Email">
-            <span class="error"></span>
-          </div>
-        </div>
-
+          <label for="email" class="col-sm-3 control-label"></label>
+        <div class="col-sm-3">
         <div class="form-group">
-          <label for="productID" class="col-sm-4 control-label">productID</label>
-          <div class="col-sm-4">
-            <input name="productID" type="text" class="form-control" columns="7" id="productID" placeholder="productID">
-            <span class="error"></span>
-          </div>
-        </div>
-
+          <label for="productID" class="col-sm-3 control-label"></label>
+        <div class="col-sm-3">
         <div class="form-group">
-      <label for="role" class="col-sm-4 control-label">rating score</label>
-      <div class="col-sm-4">
-        <select name="role" class="form-control" id="role">
-          <option value="1">very poor</option>
-          <option value="2">poor</option>
-          <option value="3">fair</option>
-          <option value="4">good</option>
-          <option value="5">very good</option>
-          <?php if ($_SESSION['role'] === 'admin') {
-            ?>
-          <option value="admin">Admin</option>
-
-
-          <?php
-        }
-          ?>
-        </select>
-        <span class="error"></span>
-      </div>
-    </div>
-
-
-          <div class="form-group">
-          <label for="feedback comment" class="col-sm-4 control-label">feedback comment</label>
-          <div class="col-sm-4">
-           <input name="feedback comment" type="text" class="form-control" id="feedback comment" placeholder="feedback comment">
-           <span class="error"></span>
-          </div>
-        </div>
-
-        <div class="form-group submit-sign-up">
-          <div class="col-sm-8 col-sm-offset-4">
-            <button type="submit" name="submit-user" value="submit-user" class="btn btn-primary">Submit</button>
-            <input type="hidden" name="doing" value="feedback">
-          </div>
-        </div>
-
-      </fieldset>
+          <label for="rating score" class="col-sm-3 control-label"></label>
+        <div class="col-sm-3">
+        <div class="form-group">
+          <label for="feedback comment" class="col-sm-3 control-label"></label>
+        <div class="col-sm-3">
+        
+        <p>userID: <input name="userID" type="text" value="" size="30" required /><br /><p>
+        email: <input name="email" type="email" value="" size="30"  required /><br />
+        productID: <input name="productID" type="text" value="" size="30" required /><br />
+        rating score:
+            1 <input type="radio" name="rating" value="1">
+            2 <input type="radio" name="rating" value="2">
+            3 <input type="radio" name="rating" value="3">
+            4 <input type="radio" name="rating" value="4">
+            5 <input type="radio" name="rating" value="5" checked> <br>
+        feedback comment:<br>
+        <input name="comments" rows="7" cols="30"><br>
+        <input name="productID" type="hidden" value="1"/>
+        <input name="userID" type="hidden" value="$_SESSION['userID']"/>
+        <input name="doing" type="hidden" value="feedback"/>
+        <input type="submit" value="submit" name="submit-feedback"/>
     </form>
-
-
-    </div>
-
-
-  </div>
-</div>
 </body>
+</html>
+
+<?php
+if (isset($_POST['submit-feedback'])) {
+$stmt = $con->prepare("INSERT INTO feedback (rating, comments)
+VALUES (?,?)");
+$stmt->bind_param("ss", $_POST['rating'], $_POST['comments']);
+$stmt->execute();
+echo "done";
+$con->close();
+}
+?>
+
 
   <footer>
   </footer>
@@ -132,3 +99,43 @@ echo $_SESSION['role'];
 
 
 </html>
+
+
+<?php
+ require '../assets/php/connect.php';
+
+ if($_POST['doing'] == "feedback"){
+
+    if(!isset($_POST['userID']) || $_POST['userID']==""){
+        echo "userID is required";
+    }elseif(!isset($_POST['email']) || $_POST['email']==""){
+        echo "email is required";
+    }elseif(!isset($_POST['productID']) || $_POST['productID']==""){
+        echo "producID is required";
+    }else{
+        $result = $link -> query("SELECT * FROM feedback WHERE userID = '".$_POST['userID']."' AND productID = '".$_POST['productID']."'");
+        if( $result && $result -> rowCount() > 0){
+            echo "You already submit";
+        }else{
+      $result = $link -> prepare("INSERT INTO feedback ( userID, email, productID, ratingScore, feedbackComment ) VALUES (:productID, :email, :productID, :ratingScore, :feedbackComment)");
+			$result -> execute(array("userID"=>$_POST['userID'],"email"=>$_POST['email'],"productID"=>$_POST['productID'], "ratingScore"=>$_POST['ratingScore'], "feedbackComment"=>$_POST['feedbackComment']));
+			if($result && $result -> rowCount() > 0){
+                echo "thank you for your feedback";
+                $to = $_POST["email"];
+                $fromName = "Webid";
+                $subject = "thank you for your feedback";
+                $message = "We will contact you as soon as possiable, thank you ";
+                $headers = "From: $fromName <test@test.copm>\r\n". 
+                            "MIME-Version: 1.0"."\r\n" . 
+                            "Content-type: text/html; charset=UTF-8"."\r\n"; 
+                @mail($to, $subject, $message, $headers); 
+            }else{
+                echo "please submit again pr contcat system manager";
+            }
+        }
+    } 
+}elseif($_POST['doing']=='feedback'){
+    // 寫入資料庫
+}else{
+    
+}
