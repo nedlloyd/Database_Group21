68b6c1082abb02c69f8ec24b4eddd9a768015d81
Yi-Hsuan CHIANG <bb33882011@gmail.com>
2018-03-08 15:26:18 +0000

    feedback

--- views/feedback.php
+++ views/feedback.php
@@ -39,6 +39,7 @@ require '../assets/php/connect.php';
 </head>
 <body>
     <form method="POST" enctype="multipart/form-data">       <!-- form delect action="do.php", add method-->                             
+        
         <div class="form-group">
           <label for="userID" class="col-sm-3 control-label"></label>
         <div class="col-sm-3">
@@ -46,7 +47,7 @@ require '../assets/php/connect.php';
           <label for="email" class="col-sm-3 control-label"></label>
         <div class="col-sm-3">
         <div class="form-group">
-          <label for="productID" class="col-sm-3 control-label"></label>
+          <label for="purchaseID" class="col-sm-3 control-label"></label>
         <div class="col-sm-3">
         <div class="form-group">
           <label for="rating score" class="col-sm-3 control-label"></label>
@@ -55,10 +56,10 @@ require '../assets/php/connect.php';
           <label for="feedback comment" class="col-sm-3 control-label"></label>
         <div class="col-sm-3">
         
-        <p>userID: <input name="userID" type="text" value="" size="30" required /><br /><p>
-        email: <input name="email" type="email" value="" size="30"  required /><br />
-        productID: <input name="productID" type="text" value="" size="30" required /><br />
-        rating score:
+        <p>*userID: <input name="userID" type="text" value="" size="30"  required /><br /><p>
+        *email: <input name="email" type="email" value="" size="30"  required /><br />
+        *purchaseID: <input name="purchaseID" type="text" value="" size="30" required /><br />
+        *rating score:
             1 <input type="radio" name="rating" value="1">
             2 <input type="radio" name="rating" value="2">
             3 <input type="radio" name="rating" value="3">
@@ -66,7 +67,7 @@ require '../assets/php/connect.php';
             5 <input type="radio" name="rating" value="5" checked> <br>
         feedback comment:<br>
         <input name="comments" rows="7" cols="30"><br>
-        <input name="productID" type="hidden" value="1"/>
+        
         <input name="userID" type="hidden" value="$_SESSION['userID']"/>
         <input name="doing" type="hidden" value="feedback"/>
         <input type="submit" value="submit" name="submit-feedback"/>
@@ -99,43 +100,3 @@ $con->close();
 
 
 </html>
-
-
-<?php
- require '../assets/php/connect.php';
-
- if($_POST['doing'] == "feedback"){
-
-    if(!isset($_POST['userID']) || $_POST['userID']==""){
-        echo "userID is required";
-    }elseif(!isset($_POST['email']) || $_POST['email']==""){
-        echo "email is required";
-    }elseif(!isset($_POST['productID']) || $_POST['productID']==""){
-        echo "producID is required";
-    }else{
-        $result = $link -> query("SELECT * FROM feedback WHERE userID = '".$_POST['userID']."' AND productID = '".$_POST['productID']."'");
-        if( $result && $result -> rowCount() > 0){
-            echo "You already submit";
-        }else{
-      $result = $link -> prepare("INSERT INTO feedback ( userID, email, productID, ratingScore, feedbackComment ) VALUES (:productID, :email, :productID, :ratingScore, :feedbackComment)");
-			$result -> execute(array("userID"=>$_POST['userID'],"email"=>$_POST['email'],"productID"=>$_POST['productID'], "ratingScore"=>$_POST['ratingScore'], "feedbackComment"=>$_POST['feedbackComment']));
-			if($result && $result -> rowCount() > 0){
-                echo "thank you for your feedback";
-                $to = $_POST["email"];
-                $fromName = "Webid";
-                $subject = "thank you for your feedback";
-                $message = "We will contact you as soon as possiable, thank you ";
-                $headers = "From: $fromName <test@test.copm>\r\n". 
-                            "MIME-Version: 1.0"."\r\n" . 
-                            "Content-type: text/html; charset=UTF-8"."\r\n"; 
-                @mail($to, $subject, $message, $headers); 
-            }else{
-                echo "please submit again pr contcat system manager";
-            }
-        }
-    } 
-}elseif($_POST['doing']=='feedback'){
-    // 寫入資料庫
-}else{
-    
-}
