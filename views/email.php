27adc1f18302b13b58fab6f35a9819946afef989
wgkv0 <wgkv0@github.com>
2018-03-09 14:09:04 +0000

    email

---
+++ views/email.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="mailto:esway@mail.com" method="post" enctype="text/plain">
+
+Name:<br>
+<input type="text" name="name"><br>
+Your e-mail address:<br>
+<input type="text" name="email" size="30"><br><br>
+Subject:<br>
+<input type="text" name="subject" size="30"><br><br>
+Comment:<br>
+<input type="text" name="comment" size="200"><br><br>
+
+<input type="submit" value="Send">
+</form>
+
+</body>
+</html>
